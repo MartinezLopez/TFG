8844ded5bd177bb83dffafd2ce945931eea77a5c
Yuri Numerov <achi@hush.ai>
2015-01-17 04:06:49 +0100

    overall syntax and readability changes

--- Modbus.py
+++ Modbus.py
@@ -8,18 +8,19 @@ import modbus_tk.modbus_rtu as modbus_rtu
 class Modbus:
   
   def __init__(self):
+
     uart.setup("UART1")
-    
     self.master = modbus_rtu.RtuMaster(serial.Serial(port="/dev/ttyO1", baudrate=9600, bytesize=8, parity='N', stopbits=1, xonxoff=0))
     self.master.set_timeout(5.0)
     self.master.set_verbose(False)
   
   def write_registers(self, slaveAddress, firstRegister, data):
+
     self.master.execute(slaveAddress, cst.WRITE_MULTIPLE_REGISTERS, firstRegister, output_value=data)
   
   def read_registers(self, slaveAddress, firstRegister, numRegisters):
-    val = self.master.execute(slaveAddress, cst.READ_HOLDING_REGISTERS, firstRegister, numRegisters)
-    return val
+
+    return self.master.execute(slaveAddress, cst.READ_HOLDING_REGISTERS, firstRegister, numRegisters)
 
 
 '''
